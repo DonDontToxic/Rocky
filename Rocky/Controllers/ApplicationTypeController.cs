dfa81c05bc4ff59be14fee64cde3d416fb0e269b
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-13 12:08:59 +0700

    Fix applicationtype model

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        // GET
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> typeList = _db.ApplicationType;
+            return View(typeList);
+        }
+        
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType type)
+        {
+            if (!ModelState.IsValid) return View(type);
+            _db.ApplicationType.Add(type);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
