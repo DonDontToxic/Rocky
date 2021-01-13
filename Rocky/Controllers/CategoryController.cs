4165c7c3a774857770205b792c253a1b24225c14
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-13 14:01:00 +0700

    Add edit and delete category feature

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -28,7 +28,6 @@ namespace Rocky.Controllers
         {
             return View();
         }
-        
         // POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -36,6 +35,49 @@ namespace Rocky.Controllers
         {
             if (!ModelState.IsValid) return View(category);
             _db.Category.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null) return NotFound();
+            
+            var category = _db.Category.Find(id); // Only find for the primary key
+            
+            if (category == null) return NotFound();
+            return View(category);
+        }
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid) return View(category);
+            _db.Category.Update(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        // GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null) return NotFound();
+            
+            var category = _db.Category.Find(id); // Only find for the primary key
+            
+            if (category == null) return NotFound();
+            return View(category);        
+        }
+        // POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _db.Category.Find(id);
+            if (category == null) return NotFound();
+            _db.Category.Remove(category);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
