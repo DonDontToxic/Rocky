e282407c46d1eb9ae136bd8098c07dbe71e29be8
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-13 11:26:15 +0700

    Initial commit

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        // GET
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            
+            return View(objList);
+        }
+        
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (!ModelState.IsValid) return View(obj);
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
