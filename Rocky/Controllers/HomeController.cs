5547ecc91313d5f98ffec7fe4c85c2906b6bbbed
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-18 13:39:21 +0700

    Modify navbar

--- Rocky/Controllers/HomeController.cs
+++ Rocky/Controllers/HomeController.cs
@@ -4,23 +4,35 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 
 namespace Rocky.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var homeVm = new HomeVM()
+            {
+                Products = _db.Product
+                    .Include(u => u.Category)
+                    .Include(u => u.ApplicationType),
+                Categories = _db.Category
+            };
+            return View(homeVm);
         }
 
         public IActionResult Privacy()
