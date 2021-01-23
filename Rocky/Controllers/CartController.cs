30a5b35636ebf10541df7714c15e7c4ac1b6960a
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-23 14:26:50 +0700

    Add cart controller and UI

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Utility;
 
 namespace Rocky.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
         public IActionResult Index()
         {
-            throw new System.NotImplementedException();
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // session exsist
+                shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>
+                    (WC.SessionCart).ToList();
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+            return View(prodList);
         }
     }
+
+
 }
