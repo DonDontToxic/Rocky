240bc1ff08429a60a923ccc190f9184bb8185d6a
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-23 14:31:52 +0700

    Handle delete cart

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -31,6 +31,21 @@ namespace Rocky.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
             return View(prodList);
         }
+
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+               && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // session exsist
+                shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>
+                    (WC.SessionCart).ToList();
+            }
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u=>u.ProductId == id))
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));        
+        }
     }
 
 
