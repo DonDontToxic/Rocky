2d8910bbd684c3147a2caa4224ad96b21ba19ced
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-25 16:02:12 +0700

    Add order page&summary

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Rocky;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 using Rocky.Utility;
 
 namespace Rocky.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
-        
+        public ProductUserVM ProductUserVm { get; set; }
+    
+
         public CartController(ApplicationDbContext db)
         {
             _db = db;
@@ -31,6 +39,41 @@ namespace Rocky.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
             return View(prodList);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+
+            return RedirectToAction(nameof(Summary));
+        }
+        
+  
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity) User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            // var userId  = User.FindFirstValue(ClaimTypes.Name);
+            
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+               && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // session exsist
+                shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>
+                    (WC.SessionCart).ToList();
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVm = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = prodList
+            };
+            return View(ProductUserVm);
+        }
 
         public IActionResult Remove(int id)
         {
@@ -42,7 +85,7 @@ namespace Rocky.Controllers
                 shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>
                     (WC.SessionCart).ToList();
             }
-            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u=>u.ProductId == id))
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u => u.ProductId == id));
             HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
             return RedirectToAction(nameof(Index));        
         }
