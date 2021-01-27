82a5663f9a15f0f92dbe2eb28d040e995dfd6bc8
Dong Nguyen <39297115+s3634096@users.noreply.github.com>
2021-01-27 17:20:40 +0700

    Add inquiry header&detail to DB

---
+++ Rocky_DataAccess/Repository/IRepository/IInquiryDetailRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Rocky_Models;
+
+namespace Rocky_DataAccess.Repository.IRepository
+{
+    public interface IInquiryDetailRepository : IRepository<InquiryDetail>
+    {
+        void Update(InquiryDetail obj);
+
+    }
+}
---
+++ Rocky_DataAccess/Repository/IRepository/IInquiryHeaderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Rocky_Models;
+
+namespace Rocky_DataAccess.Repository.IRepository
+{
+    public interface IInquiryHeaderRepository : IRepository<InquiryHeader>
+    {
+        void Update(InquiryHeader obj);
+
+    }
+}
